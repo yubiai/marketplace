f6dc5bf41084a2a2e004cf4d94c4265594138dae
David Rivero <david.davidsociety.rivero@gmail.com>
2021-07-26 21:20:11 -0300

    feat: Create profile view.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import ProfileView from './Views/Profile/ProfileView';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/profile">
+          <ProfileView />
+        </Route>
+        <Route path="/">
+          <App />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
