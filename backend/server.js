7d56b2221ba6af61ae9ab05a743200cdd118d2b7
Fernando Lopez <lopezfer@globalhitss.com>
2021-08-10 23:01:54 -0300

    refactor

---
+++ backend/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const ethers = require("ethers");
+const PaymentProcessor = require("../frontend/src/contracts/PaymentProcessor.json");
+const { Payment } = require("./models/Payment");
+
+const app = express();
+const item = require("./routes/item/item");
+const config = require("./db");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api", item);
+
+app.listen(4000, () => {
+  console.log("Server running on port 4000");
+});
+
+const listenToEvents = () => {
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://localhost:9545"
+  );
+  const networkId = "5777";
+
+  const paymentProcessor = new ethers.Contract(
+    PaymentProcessor.networks[networkId].address,
+    PaymentProcessor.abi,
+    provider
+  );
+
+  paymentProcessor.on("PaymentDone", async (payer, amount, paymentId, date) => {
+    console.log(`
+          from ${payer}
+          amount ${amount}
+          paymentId ${paymentId}
+          date ${new Date(date.toNumber() * 1000).toLocaleString()}
+        `);
+    const payment = await Payment.findOne({ id: paymentId });
+    if (payment) {
+      payment.paid = true;
+      await payment.save();
+    }
+  });
+};
+
+listenToEvents();
