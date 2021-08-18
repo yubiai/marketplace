0a43a79b9b688d186fc416210c96aa729288c77e
Fernando Lopez <lopezfer@globalhitss.com>
2021-08-18 17:41:59 -0300

    feat: add upload files middleware

--- backend/routes/profile/profile.js
+++ backend/routes/profile/profile.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const profileController = require("../../controllers/profile.controller");
+const upload = require("../../middlewares/upload");
 
 router.route("/:walletAddress")
-    .get(profileController.getProfile);
+    .get(profileController.getProfile, upload);
 
 router.route("/login")
     .post(profileController.login);
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const ethers = require("ethers");
 const PaymentProcessor = require("../frontend/src/contracts/PaymentProcessor.json");
@@ -14,7 +15,7 @@ const config = require("./db");
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use("/api/items", item);
 app.use("/api/profiles", profile);
 
