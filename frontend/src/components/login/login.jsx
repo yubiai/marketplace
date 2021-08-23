2159786fc4a60d2ef8f4dd2e6d0766d674bd8e69
Fernando Lopez <lopezfer@globalhitss.com>
2021-08-23 15:56:29 -0300

    fixs login button

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,38 +1,40 @@
-import React from 'react';
-import axios from 'axios';
-import { setupEthState } from '../../ethereum';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { setupEthState } from "../../ethereum";
 
-const API_URL = 'http://localhost:4000';
+const API_URL = "http://localhost:4000";
 
+const Login = () => {
+  const [token, setToken] = useState("");
 
-export default class Login extends React.Component {
-    /**
-     * Check if Metamask is connected, otherwise connect
-     */
-    state = {
-        token: ''
-    };
+  async function handleLogin() {
+    if (!token) {
+      const setup = await setupEthState();
+      const login = await axios.post(`${API_URL}/api/profiles/login`, {
+        walletAddress: setup.signerAddress,
+      });
 
-    x = () => {
-        if (!this.state.token) {
-            setupEthState().then(r => {
-                const { signerAddress } = r;
-                axios.post(
-                    `${API_URL}/api/profiles/login`,
-                    {
-                        walletAddress: signerAddress
-                    }
-                ).then(rx => console.log('Result.. ', rx));
-            });
-        }
+      setToken(login.data?.token);
     }
+    // setupEthState().then((r) => {
+    //   const { signerAddress } = r;
+    //   axios
+    //     .post(`${API_URL}/api/profiles/login`, {
+    //       walletAddress: signerAddress,
+    //     })
+    //     .then((rx) => console.log("Result.. ", rx));
+    // });
+  }
 
-    render() {
-        return (
-            <div className="login">
-                <h3>Login</h3>
-                <button onClick={this.x}>Click login</button>
-            </div>
-        );
-    }
-}
+  return (
+    <div className="login">
+      <h3>Login</h3>
+      <button onClick={handleLogin}>
+        {" "}
+        {token ? "Hola Usuario " : "Click login"}
+      </button>
+    </div>
+  );
+};
+
+export default Login;
