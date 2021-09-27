c530eef54560c359769afad143b7a58c044a572e
David Rivero <david.davidsociety.rivero@gmail.com>
2021-09-27 15:35:20 -0300

    feat: Add NOTES.md for .gitignore and poc of swap checkout module.

---
+++ frontend/src/components/checkout/swap-checkout.js
@@ -0,0 +1,21 @@
+export function checkWalletForUBIs(wallet) {
+    /**
+     * 1. Check all tokens available on Wallet, on ETH network
+     *  (create whitelist of tokens (avoid scam-tokens))
+     */
+}
+
+export function doSwap(amountDestination, currencyOrigin, currencyDestination) {
+    /**
+     * 1. Convert amountDestination to currency origin
+     * 2. Implement swap => Or do swap through Smart Contract
+     * (based on Santiago Siri idea)
+     * 3. Use https://github.com/Urubit/urubit.sol/blob/main/Urubit.sol as an example
+     */
+    // SimpleSWAP: https://api.simpleswap.io/#api-API_V1-CreateExchange
+    /**
+     * Uniswap:
+     * - https://docs.uniswap.org/protocol/V2/concepts/core-concepts/swaps
+     * - https://docs.uniswap.org/sdk/2.0.0/guides/trading
+     */
+}
