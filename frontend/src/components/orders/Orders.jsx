33c3bb69fe4ff67f00a4ef0aab745a048fe8f26a
Nachobr <55702500+Nachobr@users.noreply.github.com>
2021-10-14 19:38:12 -0300

    feat: style breadcrumb orders

--- frontend/src/components/orders/Orders.jsx
+++ frontend/src/components/orders/Orders.jsx
@@ -44,7 +44,7 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     '&:hover': {
         borderBottom: 'none',
-        color: '#008968',
+        color: '#00ABD1',
         textDecorationColor: 'transparent', 
       },
     //   [theme.breakpoints.down('xs')]: {
