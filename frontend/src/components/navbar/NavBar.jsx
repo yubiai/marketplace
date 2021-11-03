d1af5b3ee4f0638f47dfa05d7cd3ee867ffe8cc4
Fernando Lopez <fer.ragnar19@gmail.com>
2021-11-03 20:40:44 -0300

    prueba 0.1

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { alpha, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
