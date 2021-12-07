ff24e186625dbfa4b6688fee648d5ee12d2f31a5
dev.venezia <dev.venezia@gmail.com>
2021-12-07 13:25:13 -0300

    YUB-9 Add timestamps models message.

--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -1,35 +1,37 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const messageSchema = new Schema({
-  order_id: {
-    type: String,
-    required: true
+const messageSchema = new Schema(
+  {
+    order_id: {
+      type: String,
+      required: true,
+    },
+    buyer_user: {
+      type: String,
+      required: true,
+    },
+    seller_user: {
+      type: String,
+      required: true,
+    },
+    enabled: {
+      type: Boolean,
+      default: true,
+    },
+    messages: [
+      {
+        date: String,
+        user: String,
+        message: String,
+      },
+    ],
   },
-  buyer_user: {
-    type: String,
-    required: true
-  },
-  seller_user: {
-    type: String,
-    required: true
-  },
-  enabled: {
-    type: Boolean,
-    default: true,
-  },
-  messages: [
-    {
-      date: String,
-      user: String,
-      message: String
-    }
-  ],
   {
     versionKey: false,
     timestamps: true,
   }
-});
+);
 
 const Message = mongoose.model("Message", messageSchema);
 
