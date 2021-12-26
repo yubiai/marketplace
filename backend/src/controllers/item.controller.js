8ec66690b20dc20c7567d860c30c07783e41dedd
dev.venezia <dev.venezia@gmail.com>
2021-12-26 12:58:19 -0300

    YUB-14 Format code item controller and timestamp model Item.

--- backend/src/controllers/item.controller.js
+++ backend/src/controllers/item.controller.js
@@ -1,26 +1,26 @@
-const { Category } = require('../models/Category')
-const { Item } = require('../models/Item')
-const { Payment } = require('../models/Payment')
+const { Category } = require("../models/Category");
+const { Item } = require("../models/Item");
+const { Payment } = require("../models/Payment");
 
 const items = {
-  '1': { id: 1, url: 'http://UrlToDownloadItem1' },
-  '2': { id: 2, url: 'http://UrlToDownloadItem2' },
-}
+  1: { id: 1, url: "http://UrlToDownloadItem1" },
+  2: { id: 2, url: "http://UrlToDownloadItem2" },
+};
 
 // All get Products
 async function getItem(req, res) {
   try {
-    const item = await Item.find({})
+    const item = await Item.find({});
 
     res.status(200).json({
-      status: 'ok',
+      status: "ok",
       result: item,
-    })
+    });
   } catch (error) {
     res.status(400).json({
-      message: 'Ups Hubo un error!',
+      message: "Ups Hubo un error!",
       error: error,
-    })
+    });
   }
 }
 
@@ -29,12 +29,12 @@ async function postItem(req, res) {
   try {
     const searchCategory = await Category.find({
       categoryId: req.query.categoryId,
-    })
+    });
 
     const reqFiles = [];
 
     for (let index = 0; index < req.files.length; index++) {
-      reqFiles.push(req.files[index].filename)
+      reqFiles.push(req.files[index].filename);
     }
 
     const item = new Item({
@@ -43,83 +43,82 @@ async function postItem(req, res) {
       description: req.body.description,
       condition: req.body.condition,
       picture: reqFiles,
-    })
+    });
 
     searchCategory.map(async (e) => {
-      item.category.push(e._id)
-      // Hardcode?
-      const pepe = await Category.findById(e._id)
-      pepe.items.push(item)
-      await pepe.save()
-      await item.save()
-    })
+      item.category.push(e._id);
+      let category = await Category.findById(e._id);
+      category.items.push(item);
+      await category.save();
+      await item.save();
+    });
 
     res.status(200).json({
-      message: 'Item agregado con éxito!',
-    })
+      message: "Item agregado con éxito!",
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(400).json({
-      message: 'Ups Hubo un error!',
+      message: "Ups Hubo un error!",
       error: error,
-    })
+    });
   }
 }
 
 // One Product by SLUG
 async function getItemSlug(req, res) {
   try {
-    const item = await Item.find({slug: req.params.slug})
+    const item = await Item.find({ slug: req.params.slug });
 
     res.status(200).json({
-      status: 'ok',
+      status: "ok",
       result: item,
-    })
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(400).json({
-      message: 'Ups Hubo un error!',
+      message: "Ups Hubo un error!",
       error: error,
-    })
+    });
   }
 }
 
 // Others functions
 async function getPaymendId(req, res, next) {
   try {
-    const paymentId = (Math.random() * 10000).toFixed(0)
+    const paymentId = (Math.random() * 10000).toFixed(0);
     const paymentData = await new Payment({
       id: paymentId,
       itemId: req.params.itemId,
       paid: false,
-    })
+    });
 
-    paymentData.save()
+    paymentData.save();
 
     res.status(200).json({
       paymentId,
-    })
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 async function getItemUrl(req, res) {
   try {
     //1. verify paymentId exist in db and has been paid
-    const payment = await Payment.findOne({ id: req.params.paymentId })
+    const payment = await Payment.findOne({ id: req.params.paymentId });
     //2. return url to download item
     if (payment && payment.paid === true) {
       res.status(200).json({
         url: items && items[payment.itemId] && items[payment.itemId].url,
-      })
+      });
     } else {
       res.status(400).json({
-        message: 'Ups hubo un error!',
-      })
+        message: "Ups hubo un error!",
+      });
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
@@ -129,4 +128,4 @@ module.exports = {
   getItem,
   postItem,
   getItemSlug,
-}
+};
--- backend/src/models/Item.js
+++ backend/src/models/Item.js
@@ -1,32 +1,38 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const slug = require('mongoose-slug-updater')
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const slug = require("mongoose-slug-updater");
 
-mongoose.plugin(slug)
+mongoose.plugin(slug);
 
-const itemSchema = new Schema({
-  title: String,
-  price: Number,
-  description: String,
-  condition: String,
-  picture: Array,
-  seller: String,
-  status: String,
-  slug: {
-    type: String,
-    unique: true,
-    slug: 'title',
-  },
-  category: [
-    {
-      type: Schema.Types.Mixed,
-      ref: 'category',
+const itemSchema = new Schema(
+  {
+    title: String,
+    price: Number,
+    description: String,
+    condition: String,
+    picture: Array,
+    seller: String,
+    status: String,
+    slug: {
+      type: String,
+      unique: true,
+      slug: "title",
     },
-  ],
-})
+    category: [
+      {
+        type: Schema.Types.Mixed,
+        ref: "category",
+      },
+    ],
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
-const Item = mongoose.model('Item', itemSchema)
+const Item = mongoose.model("Item", itemSchema);
 
 module.exports = {
   Item,
-}
+};
