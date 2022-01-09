be3fba4bce8ab0b17b60a4bd6f41d5014f84bfc6
bpieres <benjapieres@gmail.com>
2022-01-09 18:08:27 -0300

    eth wallet en myinfo

--- frontend/src/components/myinfo/MyInfo.jsx
+++ frontend/src/components/myinfo/MyInfo.jsx
@@ -357,7 +357,7 @@ export default function AlignItemsList() {
                     <ListItem style={{ height: "18px" }}>
                       <ListItemText
                         classes={{ secondary: classes.listItemText }}
-                        secondary="Full name"
+                        secondary={`${data.first_name} ${data.last_name}`}
                       />
                     </ListItem>{" "}
                     <EditIcon
@@ -371,7 +371,7 @@ export default function AlignItemsList() {
                     <ListItem style={{ height: "18px" }}>
                       <ListItemText
                         classes={{ secondary: classes.listItemText }}
-                        secondary="ID Number"
+                        secondary={`${data.dni} `}
                       />
                     </ListItem>
                     <EditIcon
@@ -385,7 +385,7 @@ export default function AlignItemsList() {
                     <ListItem style={{ height: "18px" }}>
                       <ListItemText
                         classes={{ secondary: classes.listItemText }}
-                        secondary="Shipping address"
+                        secondary={`${data.address} `}
                       />
                     </ListItem>
                     <EditIcon
@@ -399,7 +399,7 @@ export default function AlignItemsList() {
                     <ListItem style={{ height: "18px" }}>
                       <ListItemText
                         classes={{ secondary: classes.listItemText }}
-                        secondary="Cellphone number"
+                        secondary={`${data.telephone} `}
                       />
                     </ListItem>
                     <EditIcon
