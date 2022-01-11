4c085a792e3ece0a73cf6dbb69025c09ea3c0fbb
Nachobr <55702500+Nachobr@users.noreply.github.com>
2022-01-11 17:44:31 -0300

    feat: add es lang

--- frontend/src/utils/i18n.ts
+++ frontend/src/utils/i18n.ts
@@ -2,12 +2,13 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import en from "../locales/en.json";
-
+import es from "../locales/es/es.json";
 const i18nInit = () =>
   i18n.use(initReactI18next).init({
     fallbackLng: "en",
     resources: {
       en,
+      es,
     },
   });
 
