04b20627be85c3463bfce3a37233abb19885c7f6
Nachobr <55702500+Nachobr@users.noreply.github.com>
2022-01-12 15:30:09 -0300

    feat: added esp trans to store and footer

--- frontend/src/utils/i18n.ts
+++ frontend/src/utils/i18n.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from "react-i18next";
 
 import en from "../locales/en.json";
 import es from "../locales/es/es.json";
+
 const i18nInit = () =>
   i18n.use(initReactI18next).init({
     fallbackLng: "en",
