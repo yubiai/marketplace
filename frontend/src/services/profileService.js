1589678c3389824c564be5de05431d8deddb3577
David Rivero <david.davidsociety.rivero@gmail.com>
2022-01-31 23:33:34 -0300

    feat: Add endpoints on frontend.

--- frontend/src/services/profileService.js
+++ frontend/src/services/profileService.js
@@ -1,13 +1,100 @@
 import axios from "axios";
 
 export const profileService = {
-    getProfile
+  getProfile,
+  login,
+  updateProfile,
+  deleteProfile,
+  getFavorites,
+  updateFavorites,
+  getMyPurchases,
+  getMySales
 }
 
+/**
+ * Check wallet addres on PoH
+ * @param {str} walletAddress
+ */
+async function getProfile(walletAddress) {
+  return await axios.post(
+    'http://localhost:4000/api/profiles/',
+    {
+      walletAddress
+    }
+  );
+}
 
-async function getProfile(signerAddress) {
-    return await axios.post(`http://localhost:4000/api/profiles/`, {
-        walletAddress: signerAddress,
-      });
+/**
+ * Update provided profile
+ * @param {str} userId
+ * @param {Object} payload
+ */
+async function updateProfile(userId, payload = {}) {
+  return await axios.put(
+    `http://localhost:4000/api/profiles/${userId}`,
+    { ...payload }
+  );
+}
+
+/**
+ * Remove provided profile
+ * @param {str} userId
+ * @param {Object} payload
+ */
+async function deleteProfile(userId) {
+  return await axios.delete(
+    `http://localhost:4000/api/profiles/${userId}`);
+}
+
+/**
+ * Login
+ * @param {str} walletAddress
+ */
+async function login(walletAddress) {
+  return await axios.post(
+    'http://localhost:4000/api/profiles/login',
+    {
+      walletAddress
+    }
+  );
+}
+
+/**
+ * Get favorite articles from a profile
+ * @param {str} userId
+ */
+async function getFavorites(userId) {
+  return await axios.get(
+    `http://localhost:4000/api/profiles/favorites/${userId}`);
+}
+
+/**
+ * Update favorite articles from a profile
+ * @param {str} userId
+ * @param {Object} payload
+ */
+async function updateFavorites(userId, payload = {}) {
+  return await axios.put(
+    `http://localhost:4000/api/profiles/favorites/${userId}`,
+    { ...payload }
+  );
+}
+
+/**
+ * Get latest purchases from a profile
+ * @param {str} userId
+ */
+async function getMyPurchases(userId) {
+  return await axios.get(
+    `http://localhost:4000/api/profiles/my_purchases/${userId}`);
+}
+
+/**
+ * Get latest sales from a profile
+ * @param {str} userId
+ */
+async function getMySales(userId) {
+  return await axios.get(
+    `http://localhost:4000/api/profiles/my_sales/${userId}`);
 }
 
