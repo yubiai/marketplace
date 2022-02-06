6e014c01e2c215b759c8f946eff5bc62b3b77482
David Rivero <david.davidsociety.rivero@gmail.com>
2022-02-06 14:52:59 -0300

    feat: Fix state set and add logic on file input.

--- frontend/src/components/add-item/addItem.jsx
+++ frontend/src/components/add-item/addItem.jsx
@@ -7,7 +7,7 @@ import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
 import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
 import { Box, Fab, withTheme } from "@material-ui/core";
-
+import { currencies, categories, conditions } from "./addItemJSON";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -22,7 +22,6 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     marginTop:'-0.5rem',
   },
-
   listItem: {
     borderRadius: '20px',
     height:'117px',
@@ -39,9 +38,6 @@ const useStyles = makeStyles((theme) => ({
       justifyContent: 'space-around'
     },
   },
-
-
-
   image: {
     width: '80px',
     height: '80px',
@@ -60,12 +56,10 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: 'white',
     }
   },
-
   title: {
     width: '100%',
     margin:'5px',
     color:'black'
-
   },
   price: {
     width: '100%',
@@ -75,27 +69,21 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     margin:'5px'
   },
-
   condition: {
     m: 1,
     width: '100%',
     color: 'black',
     margin:'5px'
-
   },
   category: {
-
     m: 1,
     width: '100%',
     margin:'5px'
-
   },
-
   description: {
     width: '100%',
     margin:'5px'
   },
-
   imguploader: {
     width: '100%',
     margin:'5px'
@@ -117,7 +105,7 @@ const useStyles = makeStyles((theme) => ({
     '&:hover': {
       outline: 'none !important',
     }
-},
+  },
   submit: {
     width: '200px',
     margin:'10px',
@@ -130,321 +118,228 @@ const useStyles = makeStyles((theme) => ({
       '&:hover': {
         backgroundColor: 'Blue',
       }
-
   },
 }));
-
-
-const conditions = [
-  {
-    value: 'New',
-    label: 'New',
-  },
-  {
-    value: 'Used',
-    label: 'Used',
-  },
-  {
-    value: 'Refurbished',
-    label: 'Refurbished',
-  },
-
-];
-
-
-const currencies = [
-  {
-    value: 'UBI',
-    label: 'UBI',
-  },
-  {
-    value: 'ETH',
-    label: 'eth',
-  },
-  {
-    value: 'BTC',
-    label: '฿',
-  },
-  {
-    value: 'xDAI',
-    label: '¥',
-  },
-];
-
-const categories = [
-  {
-    "value": "YUBI5725",
-    "label": "Accesorios para Vehículos"
-  },
-  {
-    "value": "YUBI1512",
-    "label": "Agro"
-  },
-  {
-    "value": "YUBI1403",
-    "label": "Alimentos y Bebidas"
-  },
-  {
-    "value": "YUBI1071",
-    "label": "Animales y Mascotas"
-  },
-  {
-    "value": "YUBI1367",
-    "label": "Antigüedades y Colecciones"
-  },
-  {
-    "value": "YUBI1368",
-    "label": "Arte, Librería y Mercería"
-  },
-  {
-    "value": "YUBI1743",
-    "label": "Autos, Motos y Otros"
-  },
-  {
-    "value": "YUBI1384",
-    "label": "Bebés"
-  },
-  {
-    "value": "YUBI1246",
-    "label": "Belleza y Cuidado Personal"
-  },
-  {
-    "value": "YUBI1039",
-    "label": "Cámaras y Accesorios"
-  },
-  {
-    "value": "YUBI1051",
-    "label": "Celulares y Teléfonos"
-  },
-  {
-    "value": "YUBI1648",
-    "label": "Computación"
-  },
-  {
-    "value": "YUBI1144",
-    "label": "Consolas y Videojuegos"
-  },
-  {
-    "value": "YUBI1500",
-    "label": "Construcción"
-  },
-  {
-    "value": "YUBI1276",
-    "label": "Deportes y Fitness"
-  },
-  {
-    "value": "YUBI5726",
-    "label": "Electrodomésticos y Aires Ac."
-  },
-  {
-    "value": "YUBI1000",
-    "label": "Electrónica, Audio y Video"
-  },
-  {
-    "value": "YUBI2547",
-    "label": "Entradas para Eventos"
-  },
-  {
-    "value": "YUBI407134",
-    "label": "Herramientas"
-  },
-  {
-    "value": "YUBI1574",
-    "label": "Hogar, Muebles y Jardín"
-  },
-  {
-    "value": "YUBI1499",
-    "label": "Industrias y Oficinas"
-  },
-  {
-    "value": "YUBI1459",
-    "label": "Inmuebles"
-  },
-  {
-    "value": "YUBI1182",
-    "label": "Instrumentos Musicales"
-  },
-  {
-    "value": "YUBI3937",
-    "label": "Joyas y Relojes"
-  },
-  {
-    "value": "YUBI1132",
-    "label": "Juegos y Juguetes"
-  },
-  {
-    "value": "YUBI3025",
-    "label": "Libros, Revistas y Comics"
-  },
-  {
-    "value": "YUBI1168",
-    "label": "Música, Películas y Series"
-  },
-  {
-    "value": "YUBI1430",
-    "label": "Ropa y Accesorios"
-  },
-  {
-    "value": "YUBI409431",
-    "label": "Salud y Equipamiento Médico"
-  },
-  {
-    "value": "YUBI1540",
-    "label": "Servicios"
-  },
-  {
-    "value": "YUBI9304",
-    "label": "Souvenirs, Cotillón y Fiestas"
-  },
-  {
-    "value": "YUBI1953",
-    "label": "Otras categorías"
-  }
-];
-
 
 export default function AddItem() {
   const classes = useStyles();
   const [currency, setCurrency] = React.useState('UBI');
   const [condition, setCondition] = React.useState('New');
   const [category, setCategory] = React.useState('YUBI1648');
+  const [name, setName] = React.useState('');
+  const [description, setDescription] = React.useState('');
+  const [price, setPrice] = React.useState(0);
 
-  const handleChange1 = (event1) => {
-    setCurrency(event1.target.value1);
+  // File definitions
+  const fileInputFieldSt = React.useRef(null);
+  const fileInputFieldNd = React.useRef(null);
+  const fileInputFieldRd = React.useRef(null);
+  const [files, setFiles] = React.useState({});
+
+  const setFile = (ev, field) => {
+    setFiles({
+      ...files,
+      [field]: ev.currentTarget.files[0]
+    })
   };
-  const handleChange2 = (event2) => {
-    setCondition(event2.target.value2);
-  };
-  const handleChange3 = (event3) => {
-    setCategory(event3.target.value3);
-  };
 
-
-
-return (
-
-
-
-
-  <Grid container spacing={2}
-            variant="fullWidth"
-
-            alignItems="left" style={{marginTop: '4px' ,  marginLeft: '1rem'}}>
-
-
-
-    {/* <form noValidate autoComplete="off"> */}
-
-  <Grid item xs={10} md={5} style={{backgroundColor: 'white', borderRadius:'20px' }}>
-   {/* <InputLabel id="condition">Condition</InputLabel> */}
-    <Select
-          labelId="demo-simple-select-label"
-          id="condition"
-          value={condition}
-          onChange={handleChange2}
-          className={classes.condition}
-          label="Condition"
-          variant="outlined"
-          helperText="Please select your condition"
-        >{conditions.map((option) => (
-          <MenuItem key={option.value} value={option.value}>
-            {option.label}
-          </MenuItem>
-        ))}
-
-    </Select>
-    </Grid>
-
-    <Grid item xs={10} md={5} >
-    {/* <InputLabel id="category">Category</InputLabel> */}
-    <Select
-          labelId="demo-simple-select-label"
-          id="category"
-          value={category}
-          onChange={handleChange3}
-          className={classes.category}
-          label="Category"
-          variant="outlined"
-        >{categories.map((option) => (
-          <MenuItem key={option.value} value={option.value}>
-            {option.label}
-          </MenuItem>
-        ))}
-
+  return (
+    <Grid container spacing={2}
+          variant="fullWidth"
+          alignItems="left" style={{marginTop: '4px' ,  marginLeft: '1rem'}}>
+      {/* <form noValidate autoComplete="off"> */}
+      <Grid item xs={10} md={5} style={{backgroundColor: 'white', borderRadius:'20px' }}>
+      {/* <InputLabel id="condition">Condition</InputLabel> */}
+        <Select
+              labelId="demo-simple-select-label"
+              id="condition"
+              value={condition}
+              onChange={ev => setCondition(ev.target.value)}
+              className={classes.condition}
+              label="Condition"
+              variant="outlined"
+              helperText="Please select your condition"
+            >{conditions.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
         </Select>
-    </Grid>
+      </Grid>
 
-<Grid item xs={10} md={10} >
-  <TextField id="title" label="enter item name, model & manufacturer"  type="text" name="title" className={classes.title} variant="outlined" defaultValue="Title"  />
-  </Grid>
-  <Grid item xs={10} md={10}>
-        <TextField id="description" aria-label="minimum height" minRows={4} multiline defaultValue="Description" className={classes.description} variant="outlined" />
-     </Grid>
+      <Grid item xs={10} md={5} >
+        {/* <InputLabel id="category">Category</InputLabel> */}
+        <Select
+              labelId="demo-simple-select-label"
+              id="category"
+              value={category}
+              onChange={ev => setCategory(ev.target.value)}
+              className={classes.category}
+              label="Category"
+              variant="outlined"
+            >{categories.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </Select>
+      </Grid>
 
-  <Grid item xs={10} md={3} >
-  <TextField
-          id="standard-select-currency"
-          select
-          className={classes.currency}
-          label="Currency"
-          value={currency}
-          variant="outlined"
-          onChange={handleChange1}
-           type="text"
-        >{currencies.map((option) => (
-          <MenuItem key={option.value} value={option.value}>
-            {option.label}
-          </MenuItem>
-        ))}
-    </TextField>
-    </Grid>
+      <Grid item xs={10} md={10} >
+        <TextField id="title"
+                  label="enter item name, model & manufacturer"
+                  type="text"
+                  name="title"
+                  className={classes.title}
+                  variant="outlined"
+                  value={name}
+                  onChange={ev => setName(ev.target.value)}
+                  placeholder="Title" />
+      </Grid>
+      <Grid item xs={10} md={10}>
+        <TextField id="description"
+                    aria-label="minimum height"
+                    minRows={4}
+                    multiline
+                    placeholder="Description"
+                    onChange={ev => setDescription(ev.target.value)}
+                    value={description}
+                    className={classes.description}
+                    variant="outlined" />
+      </Grid>
+      <Grid item xs={10} md={3}>
+        <TextField
+                id="standard-select-currency"
+                select
+                className={classes.currency}
+                label="Currency"
+                value={currency}
+                variant="outlined"
+                onChange={ev => setCurrency(ev.target.value)}
+                type="text"
+              >{currencies.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+        </TextField>
+      </Grid>
+      <Grid item xs={10} md={3}>
+        <TextField id="price"
+                  label="0.00"
+                  type="number"
+                  name="price"
+                  onChange={ev => setPrice(ev.target.value)}
+                  value={price}
+                  className={classes.price}
+                  variant="outlined"
+                  placeholder="Price" />
+      </Grid>
+      <Grid item xs={10} md={10} >
+        <h3>Item Images</h3>
+        <p>Get noticed by the right buyers with visual examples of your services. Images must have a minimum width of 375px, height of 375px and must not be more than 10mb each</p>
+      </Grid>
 
-    <Grid item xs={10} md={3} >
-    <TextField id="price" label="0.00" type="number" name="price" className={classes.price} variant="outlined" defaultValue="Price" />
-    </Grid>
+      {/* <label className={classes.imguploader} htmlFor="upload-photo">
+              <input
+                  style={{ display: "none" }}
 
+                  id="upload-photo"
+                  name="upload-photo"
+                  type="file"
+              />
+              <Fab
+                className={classes.addPhotoButton}
+                component="span"
+                aria-label="add"
+                variant="extended"
+              >
+              <AddAPhotoIcon />
+              </Fab>
+      </label>    */}
+      <Grid item xs={10} md={3}>
+        <Box component="div" sx={
+          {
+            position: 'relative',
+            border: '1px dashed grey',
+            height:'150px',
+            display: 'flex'
+          }
+        }>
+          <span style={{margin: 'auto'}}>Drag &amp; drop a photo or </span>
+          <input style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            left: 0,
+            top: 0,
+            opacity: 0,
+            cursor: 'pointer'
+          }}
+          accept="image/*"
+          onChange={ev => setFile(ev, 'fileOne')}
+          type="file"
+          ref={fileInputFieldSt}
+          />
+        </Box>
+      </Grid>
+      <Grid item xs={10} md={3}>
+        <Box component="div" sx={
+          {
+            border: '1px dashed grey',
+            height:'150px',
+            display: 'flex',
+            position: 'relative'
+          }
+        }>
+          <span style={{margin: 'auto'}}>Drag &amp; drop a photo or </span>
+          <input style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            left: 0,
+            top: 0,
+            opacity: 0,
+            cursor: 'pointer'
+          }}
+          onChange={ev => setFile(ev, 'fileTwo')}
+          type="file"
+          accept="image/*"
+          ref={fileInputFieldNd} />
+        </Box>
+      </Grid>
 
-     <Grid item xs={10} md={10} >
-
-          <h3>Item Images</h3>
-          <p>Get noticed by the right buyers with visual examples of your services. Images must have a minimum width of 375px, height of 375px and must not be more than 10mb each</p>
-          </Grid>
-        {/* <label className={classes.imguploader} htmlFor="upload-photo">
-                <input
-                    style={{ display: "none" }}
-
-                    id="upload-photo"
-                    name="upload-photo"
-                    type="file"
-                />
-                <Fab
-                  className={classes.addPhotoButton}
-                  component="span"
-                  aria-label="add"
-                  variant="extended"
-                >
-                 <AddAPhotoIcon />
-                 </Fab>
-         </label>    */}
-
-
-          <Grid item xs={10} md={3} >
-          <Box component="div" sx={{ p: 3, border: '1px dashed grey',   height:'150px', paddingTop:'60px', paddingLeft:'60px' }}><Button>Drag & drop a photo or </Button></Box>
-          </Grid>
-          <Grid item xs={10} md={3} >
-          <Box component="div" sx={{ p: 3, border: '1px dashed grey',  height:'150px', paddingTop:'60px', paddingLeft:'60px' }}><Button>Drag & drop a photo or </Button></Box>
-          </Grid>
-          <Grid item xs={10} md={3} >
-          <Box component="div" sx={{ p: 3, border: '1px dashed grey',   height:'150px', paddingTop:'60px', paddingLeft:'60px' }}><Button>Drag & drop a photo or </Button></Box>
-          </Grid>
-
-         <Grid item xs={10} md={10}>
-
+        <Grid item xs={10} md={3} >
+        <Box component="div" sx={
+          {
+            border: '1px dashed grey',
+            height:'150px',
+            display: 'flex',
+            position: 'relative'
+          }
+        }>
+          <span style={{margin: 'auto'}}>Drag &amp; drop a photo or </span>
+          <input style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            left: 0,
+            top: 0,
+            opacity: 0,
+            cursor: 'pointer'
+          }}
+          type="file"
+          onChange={ev => setFile(ev, 'fileThree')}
+          type="file"
+          accept="image/*"
+          ref={fileInputFieldRd} />
+        </Box>
+      </Grid>
+      <Grid item xs={10} md={10}>
         <Button type="submit" value="Submit" variant="contained" className={classes.submit}>Preview & Submit for review</Button>
-        </Grid>
-{/* </form> */}
-<div sx={{ marginBottom: '200ch'}}>&nbsp;<br></br></div>
-</Grid>
-
-);
+      </Grid>
+      {/* </form> */}
+      <div sx={{ marginBottom: '200ch'}}>&nbsp;<br></br></div>
+    </Grid>
+  );
 }
---
+++ frontend/src/components/add-item/addItemJSON.jsx
@@ -0,0 +1,171 @@
+const conditions = [
+    {
+      value: 'New',
+      label: 'New',
+    },
+    {
+      value: 'Used',
+      label: 'Used',
+    },
+    {
+      value: 'Refurbished',
+      label: 'Refurbished',
+    },
+
+];
+
+const currencies = [
+    {
+      value: 'UBI',
+      label: 'UBI',
+    },
+    {
+      value: 'ETH',
+      label: 'eth',
+    },
+    {
+      value: 'BTC',
+      label: '฿',
+    },
+    {
+      value: 'xDAI',
+      label: '¥',
+    },
+];
+
+const categories = [
+    {
+      "value": "YUBI5725",
+      "label": "Accesorios para Vehículos"
+    },
+    {
+      "value": "YUBI1512",
+      "label": "Agro"
+    },
+    {
+      "value": "YUBI1403",
+      "label": "Alimentos y Bebidas"
+    },
+    {
+      "value": "YUBI1071",
+      "label": "Animales y Mascotas"
+    },
+    {
+      "value": "YUBI1367",
+      "label": "Antigüedades y Colecciones"
+    },
+    {
+      "value": "YUBI1368",
+      "label": "Arte, Librería y Mercería"
+    },
+    {
+      "value": "YUBI1743",
+      "label": "Autos, Motos y Otros"
+    },
+    {
+      "value": "YUBI1384",
+      "label": "Bebés"
+    },
+    {
+      "value": "YUBI1246",
+      "label": "Belleza y Cuidado Personal"
+    },
+    {
+      "value": "YUBI1039",
+      "label": "Cámaras y Accesorios"
+    },
+    {
+      "value": "YUBI1051",
+      "label": "Celulares y Teléfonos"
+    },
+    {
+      "value": "YUBI1648",
+      "label": "Computación"
+    },
+    {
+      "value": "YUBI1144",
+      "label": "Consolas y Videojuegos"
+    },
+    {
+      "value": "YUBI1500",
+      "label": "Construcción"
+    },
+    {
+      "value": "YUBI1276",
+      "label": "Deportes y Fitness"
+    },
+    {
+      "value": "YUBI5726",
+      "label": "Electrodomésticos y Aires Ac."
+    },
+    {
+      "value": "YUBI1000",
+      "label": "Electrónica, Audio y Video"
+    },
+    {
+      "value": "YUBI2547",
+      "label": "Entradas para Eventos"
+    },
+    {
+      "value": "YUBI407134",
+      "label": "Herramientas"
+    },
+    {
+      "value": "YUBI1574",
+      "label": "Hogar, Muebles y Jardín"
+    },
+    {
+      "value": "YUBI1499",
+      "label": "Industrias y Oficinas"
+    },
+    {
+      "value": "YUBI1459",
+      "label": "Inmuebles"
+    },
+    {
+      "value": "YUBI1182",
+      "label": "Instrumentos Musicales"
+    },
+    {
+      "value": "YUBI3937",
+      "label": "Joyas y Relojes"
+    },
+    {
+      "value": "YUBI1132",
+      "label": "Juegos y Juguetes"
+    },
+    {
+      "value": "YUBI3025",
+      "label": "Libros, Revistas y Comics"
+    },
+    {
+      "value": "YUBI1168",
+      "label": "Música, Películas y Series"
+    },
+    {
+      "value": "YUBI1430",
+      "label": "Ropa y Accesorios"
+    },
+    {
+      "value": "YUBI409431",
+      "label": "Salud y Equipamiento Médico"
+    },
+    {
+      "value": "YUBI1540",
+      "label": "Servicios"
+    },
+    {
+      "value": "YUBI9304",
+      "label": "Souvenirs, Cotillón y Fiestas"
+    },
+    {
+      "value": "YUBI1953",
+      "label": "Otras categorías"
+    }
+];
+
+export {
+    categories,
+    currencies,
+    conditions
+};
