eb744c55f1f9be44e9cf005dec28286b37b61f5f
Nachobr <55702500+Nachobr@users.noreply.github.com>
2022-02-07 12:25:13 -0300

    feat: added event emit PaymentDone to paymentProcessor & burnAddress(for UbiBurner

--- contracts/PaymentProcessor.sol
+++ contracts/PaymentProcessor.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+
 contract PaymentProcessor {
     address public admin;
     IERC20 public ubi;
@@ -10,17 +11,27 @@ contract PaymentProcessor {
     uint256 public DIVISOR = 10000; // constant
     uint256 public adminFee;
     uint256 public burnFee;
+    address public burnAddress;
+    
+      event PaymentDone(
+        address payer,
+        uint amount,
+        uint paymentId,
+        uint date
+    );
 
     constructor(
         address adminAddress, 
         IERC20 ubiAddress, 
         uint256 _adminFee, 
-        uint256 _burnFee
+        uint256 _burnFee,
+        address _burnAddress
     ) {
         admin = adminAddress;
         ubi = ubiAddress;
         adminFee = _adminFee;
         burnFee = _burnFee;
+        burnAddress = _burnAddress;
     }
 
     function changeAdminFee(uint256 newAdminFee) external {
@@ -36,15 +47,15 @@ contract PaymentProcessor {
     }
 
     function managePayment(uint amount, uint paymentId, address receiver) external {
-        uint256 burnAmount = amount * burnFee / DIVISOR ; // use kleros cappedMath library for this.
-        uint256 adminAmount = amount * adminFee / DIVISOR ; // use kleros cappedMath library for this.
+        uint256 burnAmount = amount  / DIVISOR * burnFee ; // use kleros cappedMath library for this, not necesary with the current version of the compiler.
+        uint256 adminAmount = amount / DIVISOR * adminFee  ; // use kleros cappedMath library for this, not necesary with the current version of the compiler.
         uint256 receiverAmount = amount - burnAmount - adminAmount;
         
-        ubi.transferFrom ( msg . sender , address(this), amount); // msg.sender is the escrow.
+        ubi.transferFrom ( msg.sender , address(this), amount); // msg.sender is the escrow.
         ubi.transfer(admin, adminAmount);
         ubi.transfer(receiver, receiverAmount);
-        ubi.burn(burnAmount);
+        ubi.transfer(burnAddress, burnAmount);
 
-          // emit an event.
+           emit PaymentDone(msg.sender, receiverAmount, paymentId, block.timestamp);
     }
 }
