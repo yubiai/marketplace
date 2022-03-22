48b2f9ebe0b27b86689cf5ab7dc18ca4ed58e518
David Rivero <david.davidsociety.rivero@gmail.com>
2022-03-21 21:42:59 -0300

    fix: Fix scroll behavior on addItem view for mobile resolution.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getBlockchain } from "./ethereum.js";
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, useLocation } from 'react-router-dom';
 import { Switch, Route } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Layout from "./components/layout/Layout";
@@ -96,6 +96,9 @@ function App() {
     return <Message />;
   }
 
+  const location = useLocation();
+  const isAddItemView = location.pathname.indexOf('addItem') !== -1;
+
   return (
     <HashRouter basename="/">
       <GlobalStyle />
@@ -104,7 +107,7 @@ function App() {
           fontFamily: "Open Sans",
           height: "100%",
         }}
-        className="App"
+        className={isAddItemView ? "App App-addItemView" : "App"}
       >
         <header className={classes.header}>
           <NavBar />
